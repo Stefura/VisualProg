361fe9fa1ed268d66737a74a8df76d2a996b6a5b
aruFesT <elenacom929@gmail.com>
2023-05-21 18:31:25 +0300

    1

---
+++ lab9-10/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        Dim i As Integer
+        For i = 0 To 100 Step 20
+            ListBox1.Items.Add(i)
+        Next i
+    End Sub
+
+End Class
---
+++ lab9-10v2/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+
+        Const Mx As Integer = 327
+        Dim i As Integer, Sum As Integer
+        i = 0 : Sum = 0
+        Do
+            i = i + 1
+            Sum = Sum + (i ^ 2)
+            ListBox1.Items.Add("i: " & i & ", Sum: " & Sum)
+        Loop While Sum < Mx - (i ^ 2)
+
+        Label1.Text = "i: " & i
+        Label2.Text = "Sum: " & Sum
+
+    End Sub
+
+
+
+End Class
